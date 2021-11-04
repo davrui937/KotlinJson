ad2885c37b44606837569144459f79d0c962c2fb
David <davrui937@alu.centroafuera.es>
2021-11-04 14:01:58 +0100

    04/11/2021 Ejemplo Json

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,4 +10,37 @@ fun main() {
     println(p2)
     println(p2.toJson())
 
-}  
+
+    var numero = p1.let {
+        println("Hola soy un let y tengo dentro a $it")
+        it.edad
+    }
+
+    var numero2 = with(p1) {
+        println("Hola soy un let y tengo dentro a $this")
+        edad
+    }
+
+    var numero3 = p1.run {
+        println("Hola soy un let y tengo dentro a $this")
+        edad
+    }
+
+    var numero4 = run {
+        println("Hola soy un let y tengo dentro a $p1")
+        p1.edad
+    }
+
+    var p3 = p1.apply {
+        println("Hola soy un let y tengo dentro a $p1")
+        edad
+    }
+
+    var p4 = p1.also {
+        println("Hola soy un let y tengo dentro a $p1")
+        it.edad
+    }
+
+    
+
+}
